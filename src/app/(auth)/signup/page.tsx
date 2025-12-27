b86c030d62fca088e2cd9d129de9117f86ae38f2
Osakwe Bonaventure <osakwebonaventure9@gmail.com>
2025-12-27 17:49:57 +0100

    Rename component files to lowercase to match imports

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -15,7 +15,7 @@ import { getUserDocument } from "@/lib/services/userService";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "@/store/store";
 import { setUser } from "@/store/slices/userSlice";
-import Logo from "@/components/Logo";
+import Logo from "@/components/logo";
 
 interface ERRORTYPE {
   emailErr?: string;
